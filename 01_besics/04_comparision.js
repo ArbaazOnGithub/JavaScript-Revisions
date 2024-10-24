d5508f5a51c35abdb0b7356b973196e544c0e2f1
Mohd Arbaaz <mohd.arbaaz.0503@gmail.com>
2024-10-24 11:39:31 +0000

    Trying to push again after failed attempts

--- 01_besics/04_comparision.js
+++ 01_besics/04_comparision.js
@@ -6,7 +6,7 @@
 
 // console.log("2" > 1);
 // console.log("02" > 1);
-/*
+
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
@@ -18,7 +18,7 @@ console.log(undefined < 0);
 // === 
 
 console.log("2" === 2);//=== Is for Strict Comparision will also compare DataTypes if it is same or not if not gives errors
-*/
+
 console.log(typeof(undefined));//undefined
 console.log(typeof(null));//object
 console.log(typeof(BigInt));//function
