d591cbc61d515248b9fe641622a828b1e4311a8f
Mohd Arbaaz <mohd.arbaaz.0503@gmail.com>
2024-10-24 11:25:21 +0000

    comparision & typeof()

---
+++ 01_besics/04_comparision.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+/*
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);//=== Is for Strict Comparision will also compare DataTypes if it is same or not if not gives errors
+*/
+console.log(typeof(undefined));//undefined
+console.log(typeof(null));//object
+console.log(typeof(BigInt));//function
+console.log(typeof(NaN));//number
+console.log(typeof(0));//number
+console.log(typeof(false));//boolean 
+
+
